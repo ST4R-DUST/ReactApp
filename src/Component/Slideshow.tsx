dad5f7a172f0bd266a8471f4d801a16bc667d783
Pame <Pamela>
2019-09-21 21:41:56 -0300

    Cambio de librería para Slideshow-component

--- src/Component/Slideshow.tsx
+++ src/Component/Slideshow.tsx
@@ -1,41 +1,25 @@
 import * as React from 'react'; 
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
+//import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+//import 'pure-react-carousel/dist/react-carousel.es.css';
+import SlideShow from 'react-image-show';
 
 export function Slideshow (){
 
-    return(
-        <CarouselProvider
-            naturalSlideWidth={100}
-            naturalSlideHeight={125}
-            totalSlides={5}
-        >
-            <Slider>
-                <Slide index={0}>
-                    <img src="https://png.pngtree.com/png-vector/20190116/ourlarge/pngtree-cartoon-hand-drawn-dalmatian-puppy-pet-dog-png-image_382100.jpg"
-                        width={200}
-                        height={250}
-                    />
-                </Slide>
-                <Slide index={1}>
-                    <img src="https://images.vexels.com/media/users/3/155867/isolated/lists/e3adbc34b91a7e4cd8b4cef9a93fc23c-dinosaur-t-rex-vector.png"/>
-                </Slide>
-                <Slide index={2}>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaCGTgxtS6QcHodLexfhEJaL19j2_-FyHRJpIMiocexVqCypIS"/>    
-                </Slide>
-                <Slide index={3}>
-                    <img src="https://petcube.com/images/pages/mobile-app/feature-instagram.svg"/>    
-                </Slide>
-                <Slide index={4}>
-                    <img src="https://images.freeimages.com/images/premium/small-comps/1322/13220712-colorful-party-cupcakes.jpg"/>
-                </Slide>
-            </Slider>
-            <ButtonBack>Atrás</ButtonBack>
-            <ButtonNext>Adelante</ButtonNext>
-        </CarouselProvider>
-    
-        
-    );    
+    return (
+        <SlideShow
+        images={["http://www.clker.com/cliparts/8/d/b/1/13683079981493157586trash-empty-icon-1.png",
+                "https://icon-library.net/images/cute-icon-png/cute-icon-png-9.jpg",
+                "https://myrealdomain.com/images/cute-instagram-icon-7.vnd",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0AwzWxVNuJJawKlyhNo6x6Q-u4O0fhYOi97Kw7F8YDunZ_ZIc"
+            ]}
+        width="920px"
+        imagesWidth="800px"
+        imagesHeight="450px"
+        imagesHeightMobile="56vw"
+        thumbnailsWidth="920px"
+        thumbnailsHeight="12vw"
+        indicators fixedImagesHeight
+      />
+    );
 }
-
 export default Slideshow;
