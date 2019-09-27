0a78cb098c47955b322d47ac9053097486e7e1bb
seba.grunt <seba.grunt@gmail.com>
2019-09-27 15:35:47 -0400

    version con props list

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
-import React,{Fragment} from 'react';
-import './css/App.css';
+import React,{ Component} from 'react';
+//import './css/App.css';
 import 'semantic-ui-css/semantic.min.css'
+import NavBar from './Component/NavBar';
 
 
-const App: React.FC = () => {
+
+export default class App extends Component {
+
+  render(){
+
+  let list_series = [
+      {
+        id: 1,
+        text: 'Breaking Bad',
+        years: 2014,
+      },
+      {
+        id: 2,
+        text: 'Los Sopranos',
+        years: 1998,
+      },
+      {
+        id: 3,
+        text: 'The Wire',
+        years: 1995,
+      }
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-
+        <NavBar list_series = {list_series}/>
       </header>
     </div>
-);
+  );
+  } 
 }
 
-export default App;
+
